73b2b9b8cc03eb691671a2a02eb0ff9fe4fd2cee
Eduard Ftemov <eft@vava.cars>
2022-06-13 17:21:29 +0300

    Added 3d model.

--- components/landing/Welcome.js
+++ components/landing/Welcome.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Image from 'next/image'
+import Ufo from './Ufo'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const Welcome = () => {
     return (
-        <div>
-            1
+        <div class="flex">
+            <div class="">
+            <Canvas class="flex-none">
+                <OrbitControls enableZoom={false}/>
+                <ambientLight intency={0.5}/>
+                <Suspense>
+                    <Ufo/>
+                </Suspense>
+            </Canvas>
+            </div>
+            <div class="flex-grow">
+                Welcome !
+            </div>
+            
         </div>
     )
 }
